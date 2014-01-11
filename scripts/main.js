7d2b90b17aaf2190acd927664f44f515ee11098c
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-01-11 19:18:32 +0200

    Optimizing code

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ var DataCollection = Backbone.Collection.extend({
   }
 });
 function getUniqueDays() {
-  var dates = [], temp = [], hum = [], previous;
+  var dates = [], previous;
   _.each(raw, function(d) {
     if(d.date !== previous) {
       previous = d.date;
@@ -21,108 +21,28 @@ function getUniqueDays() {
       return false;
     }
   });
-  return _.unique(dates, true);
+  return dates;
 }
-
 function avg(array) {
-  return Math.round( (_.reduce(array, function(memo, num){
+  return Math.round((_.reduce(array, function(memo, num){
     return memo + num;
-  }, 0) / array.length) *100 ) / 100;
+  }, 0) / array.length) * 100 ) / 100;
 };
-function getDailyMetrics(array, date) {
+function dailyAvg(date, key) {
+  return avg(_.pluck(_.where(raw, {'date':date}), key))
+}
+function getDailyMetrics(date) {
  return {
    'date': date,
-   'hum_out': avg(_.pluck(_.where(array, {'date':date}), 'hum_out')),
-   'temp_out': avg(_.pluck(_.where(array, {'date':date}), 'temp_out'))
+   'hum_out': dailyAvg(date, 'hum_out'),
+   'temp_out': dailyAvg(date, 'temp_out')
   }
 }
 function aggregateMetrics() {
   return _.map(dates, function(date) {
-    return getDailyMetrics(raw, date);
+    return getDailyMetrics(date);
   })
 };
 var dates = getUniqueDays();
-var data = new DataCollection();
-data.set(aggregateMetrics())
+var data = new DataCollection(aggregateMetrics());
 
-$(function () {
-  $('#humidity').highcharts({
-    chart: {
-      backgroundColor: '#FAFAFA'
-    },
-    xAxis: {
-      tickInterval: 3,
-      categories: dates,
-      labels: {
-        formatter: function () {
-          return new Date(this.value).toDateString();
-        }
-      }
-    },
-    yAxis: {
-      gridLineColor: '#ddd',
-      title: false,
-      labels: {
-        style:{
-          fontFamily:'Helvetica-Neue, Helvetica, Arial, sans-serif',
-          fontSize:'13px',
-          color:'#777777'
-        }
-      },
-    },
-    title: { text: '' },
-    plotOptions : {
-      series : {
-        shadow : false,
-        color: "#796466",
-        marker: {
-          fillColor: '#FFFFFF',
-          lineWidth: 2,
-          lineColor: 'red'
-        },
-        events: {
-          mouseOut: function () {
-            $('div.stats span.hum').text(0)
-            $('div.stats span.date').text('')
-            $('div.stats span.temp').text('')
-            $('div.stats i').hide();
-          }
-        }
-      }
-    },
-    series: [{
-      showInLegend: true,
-      name: 'Discomfort Index',
-      fillOpacity: 0.25,
-      type: 'area' ,
-      lineWidth:3,
-      pointPadding: 50,
-      marker: {
-        lineWidth: 3,
-        lineColor: '#47535E',
-        fillColor: 'white',
-        radius: 4
-      },
-      data: data.discomfortIndexes()
-    }],
-    tooltip: {
-      style:{display: 'none'},
-      formatter: function() {
-        var model = data.where({date: this.x})[0];
-        $('span.date').text(new Date(this.x).toDateString());
-        $('span.hum').text(model.get('hum_out'));
-        $('span.discomfort').text(this.y);
-        $('span.temp').text(model.get('temp_out'));
-        $('.stats i').hide();
-        if(this.y < 21) {
-          $('i.fa-smile-o').show()
-        } else if(this.y === 21 || this.y < 24) {
-          $('i.fa-meh-o').show()
-        } else {
-          $('i.fa-frown-o').show()
-        }
-      }
-    },
-    credits:{enabled:false}
-  });
-});
