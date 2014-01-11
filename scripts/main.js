1a272a21ee4aa8326d36c17cdec1f9c5bb65508a
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-01-11 19:25:00 +0200

    Comments and tidies up

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,49 @@
+/* Humidity meter - a project for TEIPIR
+ * Copyright (c) 2014 Dimitris Tsironis - MIT LICENSE
+ */
+
+/* Data collection to save and handle our metrics */
 var DataCollection = Backbone.Collection.extend({
+  /* Returns an array with formatted days */
   days: function() {
     return _.map(this.get('dates'), function (d) {
       return new Date(d).toDateString();
     });
   },
+  /* Returns array with DI values */
   discomfortIndexes: function () {
     return _.map(this.models, function(model) {
       var T = model.get('temp_out'), RH = model.get('hum_out');
       return Math.round(( T-0.55*(1-0.01*RH)*(T-14.4) )* 100)/100;
-    })
+    });
   }
 });
+
+/* Returns array with unique days */
 function getUniqueDays() {
   var dates = [], previous;
   _.each(raw, function(d) {
     if(d.date !== previous) {
       previous = d.date;
       dates.push(d.date);
-    } else {
-      return false;
     }
   });
   return dates;
 }
+
+/* Average helper function */
 function avg(array) {
   return Math.round((_.reduce(array, function(memo, num){
     return memo + num;
   }, 0) / array.length) * 100 ) / 100;
 };
+
+/* Calculates daily avg. for a specific date and metric */
 function dailyAvg(date, key) {
   return avg(_.pluck(_.where(raw, {'date':date}), key))
 }
+
+/* Gets daily metrics [Object] for specific date */
 function getDailyMetrics(date) {
  return {
    'date': date,
@@ -38,11 +51,15 @@ function getDailyMetrics(date) {
    'temp_out': dailyAvg(date, 'temp_out')
   }
 }
+
+/* Aggregates all daily metrics for all dates */
 function aggregateMetrics() {
   return _.map(dates, function(date) {
     return getDailyMetrics(date);
   })
 };
+
+
+/* Instantiates dates array and our data collection */
 var dates = getUniqueDays();
 var data = new DataCollection(aggregateMetrics());
-
